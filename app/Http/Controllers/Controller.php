78064c94697e023447f00b9f3081598ed26e360e
Govind Kaintura <53871055+govindkaintura@users.noreply.github.com>
2021-12-15 21:35:00 +0530

    add: swagger implementation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,35 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    /** @OA\Info(title="Personal Blog", version="0.1") */
+
+    /**
+     * @OA\Get(
+
+     *  path="/v1/user/account/validate",
+
+     *  operationId="accountValidate",
+
+     *  summary="validates an account",
+
+     *  @OA\Parameter(name="email",
+
+     *    in="query",
+
+     *    required=true,
+
+     *    @OA\Schema(type="string")
+
+     *  ),
+
+     *  @OA\Response(response="200",
+
+     *    description="Validation Response",
+
+     *  )
+
+     * )
+
+     */
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
